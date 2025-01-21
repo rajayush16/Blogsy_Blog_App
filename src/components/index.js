ae5d657e74dc4a49b4e8d72272769554de3b06d2
rajayush16 <ayush.raj12516@gmail.com>
2025-01-21 23:03:00 +0530

    Error Change

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-import Signup from "./Signup";
+import SignUp from "./SignUp";
 import Login from "./Login";
 import Button from "./Button";
 import PostForm from "./Post-form/PostForm";
